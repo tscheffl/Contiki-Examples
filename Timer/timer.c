1401b088d2516f09f28389411c5d482553884f1d
Thomas Scheffler <scheffler@beuth-hochschule.de>
2018-06-11 17:18:32 +0200

    move files

---
+++ Timer/timer.c
@@ -0,0 +1,3 @@
+#include "contiki.h"#include <stdio.h>PROCESS(pt_example, "Protothread Example Process");AUTOSTART_PROCESSES(&pt_example);PROCESS_THREAD(pt_example, ev, data){	/*Timer statisch deklariert, damit er nach Kontextwechsel noch verfügbar ist*/	static struct etimer et;	PROCESS_BEGIN();	etimer_set(&et, (CLOCK_SECOND*20));	
+	while(1) 
+	{		/*Warte 20s, solange können andere Protothreads den Prozessor nutzen*/ 		PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));		etimer_reset(&et);		printf("Timer abgelaufen, starte Timer neu...\n");  	}  	PROCESS_END();}
